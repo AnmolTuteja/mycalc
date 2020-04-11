3a51f8eaee8051c1bd11250a390fe8fcb9090fee
Anmol Tuteja <Anmol.Tuteja@iiitb.org>
2020-04-12 03:58:43 +0530

    first commit to perform add operation

---
+++ src/main/java/Calc.java
@@ -0,0 +1,11 @@
+public class Calc {
+
+    public static void main(String[] args){
+        System.out.println("6 + 3 = " + add(6, 3));
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+
+import org.junit.*;
+
+public class CalcTest {
+
+    private Calc calc;
+    @Before
+    public void setUp() {
+        calc = new Calc();
+    }
+
+    @Test
+    public void testAdd() {
+        int a = 120;
+        int b = 210;
+        int expectedResult = 330;
+        long result = calc.add(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+}
