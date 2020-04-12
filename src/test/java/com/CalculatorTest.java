6cca43a687b84ce985a7cb83e818809227391fe8
AnmolTuteja <53218901+AnmolTuteja@users.noreply.github.com>
2020-04-12 21:55:23 +0530

    Update CalculatorTest.java

--- src/test/java/com/CalculatorTest.java
+++ src/test/java/com/CalculatorTest.java
@@ -14,9 +14,9 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        int a = 120;
-        int b = 210;
-        int expectedResult = 330;
+        int a = 12;
+        int b = 21;
+        int expectedResult = 33;
         long result = calc.add(a, b);
         Assert.assertEquals(expectedResult, result);;
     }
